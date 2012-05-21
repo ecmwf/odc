951bd6c38bed6916082631a3d12c99269ba994a3
Peter Kuchta <mak@ecmwf.int>
2012-05-21 10:13:42 +0000

    Clean up
    [git-p4: depot-paths = "//depot/odb_api/main/": change = 73602]

--- src/odblib/FunctionExpression.h
+++ src/odblib/FunctionExpression.h
@@ -50,48 +50,6 @@ private:
 	FunctionExpression& operator=(const FunctionExpression&);
 };
 
-/*
-
-class FunctionFactoryBase {
-protected:
-	int    arity_;
-	string name_;
-	virtual FunctionExpression* make(const string&,const expression::Expressions&) = 0;
-
-public:
-	//FunctionFactoryBase() : name_("FunctionFactory"), arity_(-1) {}
-	FunctionFactoryBase(const string& name, int arity = -1);
-	~FunctionFactoryBase();
-
-	FunctionExpression* build(const string&, SQLExpression*);
-	FunctionExpression* build(const string&, SQLExpression*, SQLExpression*);
-	FunctionExpression* build(const string&, SQLExpression*, SQLExpression*, SQLExpression*);
-	FunctionExpression* build(const string&, const expression::Expressions&);
-};
-
-class FunctionFactory : public FunctionFactoryBase {
-public:
-	static FunctionFactory& instance();
-	FunctionFactory() : FunctionFactoryBase("FunctionFactory", -1) {}
-
-	vector<pair<string, int> >& functionsInfo();
-
-private:
-	FunctionExpression* make(const string&,const expression::Expressions&) { NOTIMP; return 0; }
-
-	map<pair<string,int>, FunctionFactoryBase*> map_;
-	vector<pair<string, int> > functionInfo_;
-};
-
-template<class T>
-class FunctionMaker : public FunctionFactoryBase {
-	FunctionExpression* make(const string& name, const expression::Expressions& args)
-	{ return new T(name, args); }
-public:
-	FunctionMaker(const string& name, int arity = -1) : FunctionFactoryBase(name, arity) {}
-};
-*/
-
 } // namespace function
 } // namespace expression 
 } // namespace sql
