a5fb2a8238cd7c398c83745bb85bf6bc369b1e76
Peter Kuchta <mak@ecmwf.int>
2012-05-24 15:06:19 +0000

    Logging formatted like in MARS again.
    [git-p4: depot-paths = "//depot/odb_api/main/": change = 73810]

--- src/odblib/ODBBehavior.cc
+++ src/odblib/ODBBehavior.cc
@@ -1,8 +1,34 @@
 #include "eclib/Logger.h"
 #include "eclib/StandardBehavior.h"
 #include "eclib/StdLogger.h"
+#include "eclib/DHSStdLogger.h"
+#include "eclib/RotLogger.h"
 
 #include "ODBBehavior.h"
 
-Logger* ODBBehavior::createInfoLogger() { return new StdLogger( std::cerr ); }
-Logger* ODBBehavior::createDebugLogger() { return new StdLogger( std::cerr ); } 
+Logger* ODBBehavior::createInfoLogger()
+{
+    DHSLogger * logger = 0;
+    if( ! toFile() )    
+        logger = new DHSStdLogger( std::cerr);
+    else
+        logger = new RotLogger(); 
+    
+    logger->prefix("(I) ");
+    
+    return logger;
+}
+
+
+Logger* ODBBehavior::createDebugLogger() 
+{    
+    DHSLogger * logger = 0;
+    if( ! toFile() )  
+        logger = new DHSStdLogger( std::cerr);
+    else
+        logger = new RotLogger();        
+    
+    logger->prefix("(D) ");
+    
+    return logger;
+}
