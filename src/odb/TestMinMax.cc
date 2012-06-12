a0339525b34d5c0b4d70691373ebcccb6ba03656
Peter Kuchta <mak@ecmwf.int>
2012-06-12 09:25:45 +0000

    Check if INT32_{MIN|MAX} already defined
    [git-p4: depot-paths = "//depot/odb_api/main/": change = 74758]

--- src/odb/TestMinMax.cc
+++ src/odb/TestMinMax.cc
@@ -21,11 +21,13 @@
 
 #include <stdint.h>
 
-#warning "INT32_MIN is system defined in stdint.h"
-#warning "INT32_MAX is system defined in stdint.h"
-
+#ifndef INT32_MIN
 # define INT32_MIN		(-2147483647-1)
+#endif
+
+#ifndef INT32_MAX
 # define INT32_MAX		(2147483647)
+#endif
 
 using namespace std;
 
