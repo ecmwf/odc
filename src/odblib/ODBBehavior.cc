050593dc3e403875af4200ce92bc780b0eb67136
Piotr Kuchta <peter@kuchta.co.uk>
2013-04-26 15:33:26 +0100

    ODB-35 Fixed warning 'DHSHOME environment variable not defined'

--- src/odblib/ODBBehavior.cc
+++ src/odblib/ODBBehavior.cc
@@ -8,9 +8,79 @@
  * does it submit to any jurisdiction.
  */
 
-#include "odblib/ODBBehavior.h"
+#include <stdlib.h>
 
-ODBBehavior::ODBBehavior():
-    DHSBehaviorStdout( std::cerr)
+#include "eclib/Application.h"
+#include "eclib/Colour.h"
+#include "eclib/DHSStdLogger.h"
+#include "eclib/Log.h"
+#include "eclib/Monitor.h"
+#include "eclib/Resource.h"
+#include "eclib/RotLogger.h"
+
+#include "ODBBehavior.h"
+
+namespace odb {
+
+ODBBehavior::ODBBehavior() : taskId_(0), out_(cerr), name_() {}
+ODBBehavior::~ODBBehavior() { eclib::Monitor::shutdown(); }
+
+void ODBBehavior::initialize()
 {
+    try
+    {
+        eclib::Monitor::active( true );
+        eclib::Monitor::startup();
+        taskId_ = eclib::Monitor::self();
+    }
+    catch(exception& e) {
+        std::cerr << "** " << e.what() << " Caught in " << Here() <<  std::endl;
+        std::cerr << "** Exception is re-thrown" << std::endl;
+        throw;
+    }
 }
+
+eclib::DHSLogger* ODBBehavior::getLogger() { return new eclib::DHSStdLogger(out_); }
+
+void ODBBehavior::finalize() {}
+
+string ODBBehavior::runName() const { return name_.size() ? name_ : eclib::Application::appName(); }
+
+void ODBBehavior::runName( const string& name ) { name_ = name; }
+
+long ODBBehavior::taskId() const { return taskId_; }
+
+eclib::Logger* ODBBehavior::createInfoLogger()
+{
+    eclib::DHSLogger * logger = getLogger();
+    logger->prefix("(I) ");
+    return logger;
+}
+
+eclib::Logger* ODBBehavior::createDebugLogger()
+{    
+    eclib::DHSLogger * logger = getLogger();
+    logger->prefix("(D) ");
+    return logger;
+}
+
+eclib::Logger* ODBBehavior::createWarningLogger()
+{    
+    eclib::DHSLogger * logger = getLogger();
+    logger->prefix("(W) ");
+    logger->setColor(&eclib::Colour::yellow);
+    logger->resetColor(&eclib::Colour::reset);
+    return logger;
+}
+
+eclib::Logger* ODBBehavior::createErrorLogger()
+{    
+    eclib::DHSLogger * logger = getLogger();
+    logger->prefix("(E) ");
+    logger->setColor(&eclib::Colour::red);
+    logger->resetColor(&eclib::Colour::reset);
+    return logger;
+}
+
+} // namespace odb 
+ 
--- src/odblib/ODBBehavior.h
+++ src/odblib/ODBBehavior.h
@@ -1,24 +1,55 @@
 /*
  * (C) Copyright 1996-2012 ECMWF.
- * 
+ *
  * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
- * In applying this licence, ECMWF does not waive the privileges and immunities 
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
  * granted to it by virtue of its status as an intergovernmental organisation nor
  * does it submit to any jurisdiction.
  */
 
-#ifndef ODBBehavior_H
-#define ODBBehavior_H
+#ifndef ODBBehavior_h
+#define ODBBehavior_h
 
-#include "eclib/StandardBehavior.h"
-#include "eclib/DHSBehaviorStdout.h"
+#include "eclib/ContextBehavior.h"
 
-class ODBBehavior : public eclib::DHSBehaviorStdout
-{
+namespace eclib {
+	class DHSLogger;
+}
+
+namespace odb {
+
+class ODBBehavior : public eclib::ContextBehavior {
 public:
     ODBBehavior();
+    ~ODBBehavior();
+    
+protected: // methods
+
+    virtual void initialize();
+    virtual void finalize();
+    
+    virtual string runName() const;
+    virtual void runName( const string& name ); 
+    
+    virtual long taskId() const;
+    
+    virtual eclib::Logger* createInfoLogger();
+    virtual eclib::Logger* createDebugLogger();
+    virtual eclib::Logger* createWarningLogger();
+    virtual eclib::Logger* createErrorLogger();
+
+    virtual eclib::DHSLogger* getLogger();
+    
+private: // members
+    
+    long taskId_;
+    ostream& out_;
+    string name_;
+    
 };
 
-#endif
+} // namespace odb
+
+#endif 
 
