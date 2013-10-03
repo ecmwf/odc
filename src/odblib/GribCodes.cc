ecmwf/odc

commit 2ffc89a8017d5f76663295e328a5f204d060e9b2

Change to use MARS_HOME and read odb_api_type.table

src/odblib/GribCodes.cc
@@ -49,14 +49,7 @@ string GribCodes::numeric(const string& keyword, const string& alphanumeric)
 {
 	load();
 	const string kw = StringTools::upper(keyword);
-	if (kw == "TYPE")
-	{
-		// HACK:
-		if (alphanumeric == "OFB") return "263";
-		if (alphanumeric == "MFB") return "262";
-
-		return typeCodes_->numeric(alphanumeric);
-	}
+	if (kw == "TYPE") return typeCodes_->numeric(alphanumeric);
 	if (kw == "CLASS") return classCodes_->numeric(alphanumeric);
 	if (kw == "STREAM") return streamCodes_->numeric(alphanumeric);
 	if (kw == "OBSGROUP") return obsgroupCodes_->numeric(alphanumeric);
@@ -69,14 +62,7 @@ string GribCodes::alphanumeric(const string& keyword, const string& numeric)
 {
 	load();
 	const string kw = StringTools::upper(keyword);
-	if (kw == "TYPE") 
-	{
-		// HACK:
-		if (numeric == "262") return "MFB";
-		if (numeric == "263") return "OFB";
-
-		return typeCodes_->alphanumeric(numeric);
-	}
+	if (kw == "TYPE") return typeCodes_->alphanumeric(numeric);
 	if (kw == "CLASS") return classCodes_->alphanumeric(numeric);
 	if (kw == "STREAM") return streamCodes_->alphanumeric(numeric);
 	if (kw == "OBSGROUP") return obsgroupCodes_->alphanumeric(numeric);
@@ -87,7 +73,7 @@ string GribCodes::alphanumeric(const string& keyword, const string& numeric)
 
 GribCodesBase::GribCodesBase(const PathName& fileName)
 : configFileName_(
-	string(Resource<string>("$ODB_API_HOME", "/usr/local/lib/metaps/lib/odalib/current"))
+	string(Resource<string>("$MARS_HOME", "/usr/local/apps/mars/current"))
 	+ string("/etc/")
 	+ fileName),
   fieldDelimiter_(" \t"),
@@ -99,7 +85,7 @@ GribCodesBase::GribCodesBase(const PathName& fileName)
 
 GribCodesBase::GribCodesBase(const PathName& fileName, const string& fieldDelimiter)
 : configFileName_(
-	string(Resource<string>("$ODB_API_HOME", "/usr/local/lib/metaps/lib/odalib/current"))
+	string(Resource<string>("$MARS_HOME", "/usr/local/apps/mars/current"))
 	+ string("/etc/")
 	+ fileName),
   fieldDelimiter_(fieldDelimiter),

src/odblib/GribCodes.h
@@ -55,7 +55,7 @@ public:
 
 class TypeCodes : public GribCodesBase {
 public:
-	TypeCodes() : GribCodesBase("type.table") {}
+	TypeCodes() : GribCodesBase("odb_api_type.table") {}
 };
 
 class StreamCodes : public GribCodesBase {

commit f326b5c6244fb27ae631c868c4788576a23b2dc9

ODB-70: oda2request was broken by change 2ffc89a

src/odblib/GribCodes.cc
@@ -49,7 +49,14 @@ string GribCodes::numeric(const string& keyword, const string& alphanumeric)
 {
 	load();
 	const string kw = StringTools::upper(keyword);
-	if (kw == "TYPE") return typeCodes_->numeric(alphanumeric);
+	if (kw == "TYPE")
+	{
+		// HACK:
+		if (alphanumeric == "OFB") return "263";
+		if (alphanumeric == "MFB") return "262";
+
+		return typeCodes_->numeric(alphanumeric);
+	}
 	if (kw == "CLASS") return classCodes_->numeric(alphanumeric);
 	if (kw == "STREAM") return streamCodes_->numeric(alphanumeric);
 	if (kw == "OBSGROUP") return obsgroupCodes_->numeric(alphanumeric);
@@ -62,7 +69,14 @@ string GribCodes::alphanumeric(const string& keyword, const string& numeric)
 {
 	load();
 	const string kw = StringTools::upper(keyword);
-	if (kw == "TYPE") return typeCodes_->alphanumeric(numeric);
+	if (kw == "TYPE") 
+	{
+		// HACK:
+		if (numeric == "262") return "MFB";
+		if (numeric == "263") return "OFB";
+
+		return typeCodes_->alphanumeric(numeric);
+	}
 	if (kw == "CLASS") return classCodes_->alphanumeric(numeric);
 	if (kw == "STREAM") return streamCodes_->alphanumeric(numeric);
 	if (kw == "OBSGROUP") return obsgroupCodes_->alphanumeric(numeric);
@@ -73,7 +87,7 @@ string GribCodes::alphanumeric(const string& keyword, const string& numeric)
 
 GribCodesBase::GribCodesBase(const PathName& fileName)
 : configFileName_(
-	string(Resource<string>("$MARS_HOME", "/usr/local/apps/mars/current"))
+	string(Resource<string>("$ODB_API_HOME", "/usr/local/lib/metaps/lib/odalib/current"))
 	+ string("/etc/")
 	+ fileName),
   fieldDelimiter_(" \t"),
@@ -85,7 +99,7 @@ GribCodesBase::GribCodesBase(const PathName& fileName)
 
 GribCodesBase::GribCodesBase(const PathName& fileName, const string& fieldDelimiter)
 : configFileName_(
-	string(Resource<string>("$MARS_HOME", "/usr/local/apps/mars/current"))
+	string(Resource<string>("$ODB_API_HOME", "/usr/local/lib/metaps/lib/odalib/current"))
 	+ string("/etc/")
 	+ fileName),
   fieldDelimiter_(fieldDelimiter),

src/odblib/GribCodes.h
@@ -55,7 +55,7 @@ public:
 
 class TypeCodes : public GribCodesBase {
 public:
-	TypeCodes() : GribCodesBase("odb_api_type.table") {}
+	TypeCodes() : GribCodesBase("type.table") {}
 };
 
 class StreamCodes : public GribCodesBase {