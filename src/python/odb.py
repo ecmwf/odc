93b87f3e44e135c2cba429ececa075c4535ce83b
Piotr Kuchta <peter.kuchta@ecmwf.int>
2014-09-17 16:10:22 +0100

    Add function to load SQL results into Pandas DataFrame

--- src/python/odb.py
+++ src/python/odb.py
@@ -10,6 +10,7 @@
 # Piotr Kuchta - ECMWF Nov 2011
 
 import pyodbapi
+import re
 
 class Select:
 	def __init__(self, sql):
@@ -30,6 +31,36 @@ def open(fileName):
 	print "odb.open: '" + fileName + "'"
 	return pyodbapi.Reader(fileName)
 
-# ex file
-fn = '../oda/some_conv_2011-01-08.odb'
+
+def type_to_name(n):
+    if n == 1: return 'INTEGER'
+    if n == 2: return 'REAL'
+    if n == 3: return 'STRING'
+    if n == 4: return 'BITFIELD'
+    raise 'Unknown type ' + str(n)
+
+def as_DataFrame(statement, convert_time=True):
+    import pandas as pd
+    import numpy as np
+    for row in sql(statement):
+        columns = [c.name() for c in row.columns()]
+        types = [c.type() for c in row.columns()]
+        print "\n".join([t[0] + ':' + type_to_name(t[1]) for t in zip(columns, types) if type_to_name(t[1]) == 'BITFIELD'])
+        break
+    rows = [r[:] for r in sql(statement)]
+    xx = pd.DataFrame.from_records(rows, columns = columns)
+    xx.rename(columns=lambda x: re.sub('@[^ ]*$','',x).strip(), inplace=True)
+    if 'statid' in xx.columns:
+        xx['statid'] = xx.statid.apply(lambda x: re.sub("'",'',x).strip())
+    if convert_time:
+        xx['date'] = pd.to_datetime(xx['date'].astype(str),format='%Y%m%d')
+        xx['time'] = (xx['time']//10000)*3600.+ (np.mod(xx['time'],10000)//100)*60+(np.mod(xx['time'],100))*60
+        # Add datetime
+        xx['datetime']=(xx['date'].values + xx['time'].values.astype('timedelta64[s]'))
+    return xx
+
+# TODO: a proper test
+#df = as_DataFrame('select * from "conv.odb";')
+#print df['report_status']
+
 
