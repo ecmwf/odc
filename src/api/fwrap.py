7fbe6aa32d64083ef6ec04c432b6518b923f8da5
Piotr Kuchta <peter.kuchta@ecmwf.int>
2016-07-26 18:15:05 +0100

    More work on Fortran wrappers generator - thanks, Tomas!

--- src/api/fwrap.py
+++ src/api/fwrap.py
@@ -35,7 +35,7 @@ def parseDeclaration(decl):
     return_type = normalize_type(return_type)
 
     params = decl.split('(',1)[1].strip().rsplit(')', 1)[0].strip()
-    params = tuple([parseParam(p) for p in params.split(',')])
+    params = list([parseParam(p) for p in params.split(',')])
     return (decl, (return_type, fun_name, params))
 
 def translate_type_for_binding(t):
@@ -55,8 +55,8 @@ def translate_type_for_binding(t):
 
 def translate_type_for_binding_return(t):
     #print 'translate_type_for_binding_return:', t
-    if t == 'const char*':           return 'character(kind=C_CHAR),dimension(*)'
-    if t == 'const unsigned char*':  return 'character(kind=C_CHAR),dimension(*)' # TODO same as signed?
+    if t == 'const char*':           return 'type(C_PTR)'
+    if t == 'const unsigned char*':  return 'type(C_PTR)'
     if t == 'int':                   return 'integer(kind=C_INT)'
 
     raise Exception("Don't know how to translate '" + t + "'")
@@ -64,14 +64,14 @@ def translate_type_for_binding_return(t):
 
 def translate_type_for_fortran(t):
     #print 'translate_type_for_fortran:', t
-    if t == 'const char*':           return 'character(kind=C_CHAR),dimension(*)'
-    if t == 'const char**':          return 'character(kind=C_CHAR),dimension(*)' # TODO
+    if t == 'const char*':           return 'character(len=*),intent(in)'
+    if t == 'const char**':          return 'character(len=*),intent(out)'
     if t == 'double':                return 'real(kind=C_DOUBLE)'
     if t == 'int':                   return 'integer(kind=C_INT)'
     if t == 'odbql*':                return 'type(C_PTR), VALUE'
-    if t == 'odbql**':               return 'real(kind=C_DOUBLE)' # TODO
+    if t == 'odbql**':               return 'type(C_PTR)'
     if t == 'odbql_stmt*':           return 'type(C_PTR), VALUE'
-    if t == 'odbql_stmt**':          return 'real(kind=C_DOUBLE)' # TODO
+    if t == 'odbql_stmt**':          return 'type(C_PTR)'
     if t == 'void(*)(void*)':        return 'real(kind=C_DOUBLE)' # TODO
 
     raise Exception("Don't know how to translate '" + t + "'")
@@ -79,8 +79,8 @@ def translate_type_for_fortran(t):
 
 def translate_type_for_fortran_return(t):
     #print 'translate_type_for_fortran_return:', t
-    if t == 'const char*':           return 'character(kind=C_CHAR),dimension(*)'
-    if t == 'const unsigned char*':  return 'character(kind=C_CHAR),dimension(*)' # TODO same as signed?
+    if t == 'const char*':           return 'character(len=*),intent(out)'
+    if t == 'const unsigned char*':  return 'character(len=*),intent(out)' # TODO: think about it
     if t == 'int':                   return 'integer(kind=C_INT)'
 
     raise Exception("Don't know how to translate '" + t + "'")
@@ -93,20 +93,39 @@ def fortranParamTypeDeclaration(p, translate_type = translate_type_for_binding):
 
 def generateWrapper(declaration, comment):
 
+    procedure_keyword = 'function'
+
     return_type, function_name, params = declaration
+
+    print 'generateWrapper: ', declaration, comment
+
+    print 'params:', params
+
+    output_parameter = function_name
 
     # filter out void from parameterless fucntions (subroutines?) e.g: const char * odbql_libversion(void)
     if len(params) == 1 and (len(params[0][0]) == 0 and params[0][1] == 'void'):
         params = []
+    params = [p for p in params if not p[1] == 'void(*)(void*)']
+
+    fortran_params = params[:]
+
+    if return_type == 'const char*' or return_type == 'const unsigned char*':
+        procedure_keyword = 'subroutine'
+        output_parameter = 'return_value'
+        fortran_params.append( (return_type, output_parameter) )
 
     parameter_list = ','.join([p[1] for p in params])
     parameter_list = '(' + parameter_list + ')'
 
+    fortran_parameter_list = ','.join([p[1] for p in fortran_params])
+    fortran_parameter_list = '(' + fortran_parameter_list + ')'
+
     binding_parameters_declarations = '\n     '.join([fortranParamTypeDeclaration(p) for p in params])
-    binding_return_type_declaration = formatParameter(translate_type_for_binding_return(return_type), function_name)
+    binding_return_type_declaration = formatParameter(translate_type_for_binding_return(return_type), function_name + '_c')
 
     fortran_parameters_declarations = '\n     '.join([fortranParamTypeDeclaration(p, translate_type = translate_type_for_fortran) for p in params])
-    fortran_return_type_declaration = formatParameter(translate_type_for_fortran_return(return_type), function_name)
+    fortran_return_type_declaration = formatParameter(translate_type_for_fortran_return(return_type), output_parameter)
 
     use_intrinsic = formatParameter('use, intrinsic', 'iso_c_binding')
 
@@ -119,14 +138,14 @@ def generateWrapper(declaration, comment):
      %(binding_return_type_declaration)s
     end function %(function_name)s_c
 
-    function %(function_name)s %(parameter_list)s 
+    %(procedure_keyword)s %(function_name)s %(fortran_parameter_list)s 
      %(use_intrinsic)s
      %(fortran_parameters_declarations)s
      %(fortran_return_type_declaration)s
 
 !>     %(function_name)s = %(function_name)s_c %(parameter_list)s
 
-    end function %(function_name)s
+    end %(procedure_keyword)s %(function_name)s
 
     """ 
     return template % locals()
