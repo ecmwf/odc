d03d363f816fe5af0754f7da32b721fca65dfef8
Piotr Kuchta <peter.kuchta@ecmwf.int>
2016-07-26 14:54:27 +0100

    Work on Fortran wrappers generator

--- src/api/fwrap.py
+++ src/api/fwrap.py
@@ -2,80 +2,130 @@ import re
 
 PARAM_TYPE_COLUMN = 37
 
+def formatParameter(typ, name):
+    global PARAM_TYPE_COLUMN 
+    space = ' ' * (PARAM_TYPE_COLUMN - len(typ)) 
+    return typ + space + ':: ' + name
+
 decls = [line for line in [l.strip() for l in open('odbql.cc').read().splitlines()]
          if line.find('odbql_') <> -1 
             and not line.startswith('//')
             and line.find('return') == -1
             and line.find('typedef') == -1]
 
+def normalize_type(t): return re.sub(' [*]', '*', t)
+
 def parseParam(p):
     p = p.strip()
+
     if p == 'void(*d)(void*)':
         return 'void(*)(void*)', 'd'
+
     param_name = re.findall(r"[\w']+", p)[-1]
-    return p[:len(p) - len(param_name)].strip(), param_name.strip()
+    param_type = p[:len(p) - len(param_name)].strip()
+    param_type = normalize_type(param_type)
+
+    return param_type, param_name
     
 
 def parseDeclaration(decl):
     ret_and_fun_name = decl.split('(', 1)[0].strip()
     fun_name = re.findall(r"[\w']+", ret_and_fun_name)[-1]
     return_type = ret_and_fun_name[:-len(fun_name)].strip()
+    return_type = normalize_type(return_type)
 
     params = decl.split('(',1)[1].strip().rsplit(')', 1)[0].strip()
     params = tuple([parseParam(p) for p in params.split(',')])
     return (decl, (return_type, fun_name, params))
 
-def translate_type(t):
-    if t == 'int':                  return 'integer(kind=C_INT),VALUE'
-    if t == 'double':               return 'real(kind=C_DOUBLE),VALUE'
-    if t == 'const char *':         return 'character(kind=C_CHAR),dimension(*)'
-    if t == 'const char*':          return 'character(kind=C_CHAR),dimension(*)'
-    if t == 'const unsigned char*': return 'character(kind=C_CHAR),dimension(*)' # TODO same as signed?
-    if t == 'const unsigned char *': return 'character(kind=C_CHAR),dimension(*)' # TODO same as signed?
-    if t == 'const char **':        return 'character(kind=C_CHAR),dimension(*)' # TODO
-    if t == 'odbql*':               return 'type(C_PTR), VALUE'
-    if t == 'odbql *':              return 'type(C_PTR), VALUE'
-    if t == 'odbql **':             return 'real(kind=C_DOUBLE)' # TODO
-    if t == 'odbql_stmt*':          return 'type(C_PTR), VALUE'
-    if t == 'odbql_stmt *':         return 'type(C_PTR), VALUE'
-    if t == 'odbql_stmt **':        return 'real(kind=C_DOUBLE)' # TODO
-    if t == 'void(*)(void*)':       return 'real(kind=C_DOUBLE)' # TODO
+def translate_type_for_binding(t):
+    #print 'translate_type_for_binding:', t
+    if t == 'const char*':           return 'character(kind=C_CHAR),dimension(*)'
+    if t == 'const char**':          return 'character(kind=C_CHAR),dimension(*)' # TODO
+    if t == 'double':                return 'real(kind=C_DOUBLE)'
+    if t == 'int':                   return 'integer(kind=C_INT)'
+    if t == 'odbql*':                return 'type(C_PTR), VALUE'
+    if t == 'odbql**':               return 'real(kind=C_DOUBLE)' # TODO
+    if t == 'odbql_stmt*':           return 'type(C_PTR), VALUE'
+    if t == 'odbql_stmt**':          return 'real(kind=C_DOUBLE)' # TODO
+    if t == 'void(*)(void*)':        return 'real(kind=C_DOUBLE)' # TODO
 
     raise Exception("Don't know how to translate '" + t + "'")
 
-def fortranParamTypeDeclaration(p):
+
+def translate_type_for_binding_return(t):
+    #print 'translate_type_for_binding_return:', t
+    if t == 'const char*':           return 'character(kind=C_CHAR),dimension(*)'
+    if t == 'const unsigned char*':  return 'character(kind=C_CHAR),dimension(*)' # TODO same as signed?
+    if t == 'int':                   return 'integer(kind=C_INT)'
+
+    raise Exception("Don't know how to translate '" + t + "'")
+
+
+def translate_type_for_fortran(t):
+    #print 'translate_type_for_fortran:', t
+    if t == 'const char*':           return 'character(kind=C_CHAR),dimension(*)'
+    if t == 'const char**':          return 'character(kind=C_CHAR),dimension(*)' # TODO
+    if t == 'double':                return 'real(kind=C_DOUBLE)'
+    if t == 'int':                   return 'integer(kind=C_INT)'
+    if t == 'odbql*':                return 'type(C_PTR), VALUE'
+    if t == 'odbql**':               return 'real(kind=C_DOUBLE)' # TODO
+    if t == 'odbql_stmt*':           return 'type(C_PTR), VALUE'
+    if t == 'odbql_stmt**':          return 'real(kind=C_DOUBLE)' # TODO
+    if t == 'void(*)(void*)':        return 'real(kind=C_DOUBLE)' # TODO
+
+    raise Exception("Don't know how to translate '" + t + "'")
+
+
+def translate_type_for_fortran_return(t):
+    #print 'translate_type_for_fortran_return:', t
+    if t == 'const char*':           return 'character(kind=C_CHAR),dimension(*)'
+    if t == 'const unsigned char*':  return 'character(kind=C_CHAR),dimension(*)' # TODO same as signed?
+    if t == 'int':                   return 'integer(kind=C_INT)'
+
+    raise Exception("Don't know how to translate '" + t + "'")
+
+def fortranParamTypeDeclaration(p, translate_type = translate_type_for_binding):
     global PARAM_TYPE_COLUMN
     typ, parameter_name = p
     fortran_type = translate_type(typ)
-    space = ' ' * (PARAM_TYPE_COLUMN - len(fortran_type))
-    return  fortran_type + space + ':: ' + parameter_name
+    return formatParameter(fortran_type, parameter_name)
 
+def generateWrapper(declaration, comment):
 
-def generateWrapper(d, comment):
-    global PARAM_TYPE_COLUMN 
+    return_type, function_name, params = declaration
 
-    return_type, function_name, params = d
-
-    parameter_declarations = [p for p in params
-                                if not (p[1] == 'void' and len(p[0]) == 0)]
-    parameter_declarations = '\n     '.join([fortranParamTypeDeclaration(p) 
-                                        for p in parameter_declarations])
+    # filter out void from parameterless fucntions (subroutines?) e.g: const char * odbql_libversion(void)
+    if len(params) == 1 and (len(params[0][0]) == 0 and params[0][1] == 'void'):
+        params = []
 
     parameter_list = ','.join([p[1] for p in params])
-    if parameter_list: parameter_list = '(' + parameter_list + ')'
+    parameter_list = '(' + parameter_list + ')'
 
-    space = ' ' * (PARAM_TYPE_COLUMN - len('use, intrinsic')) 
+    binding_parameters_declarations = '\n     '.join([fortranParamTypeDeclaration(p) for p in params])
+    binding_return_type_declaration = formatParameter(translate_type_for_binding_return(return_type), function_name)
 
-    fortran_return_type = translate_type(return_type)
-    return_type_declaration = fortran_return_type + (' ' * (PARAM_TYPE_COLUMN - len(fortran_return_type))) + ":: " + function_name 
+    fortran_parameters_declarations = '\n     '.join([fortranParamTypeDeclaration(p, translate_type = translate_type_for_fortran) for p in params])
+    fortran_return_type_declaration = formatParameter(translate_type_for_fortran_return(return_type), function_name)
+
+    use_intrinsic = formatParameter('use, intrinsic', 'iso_c_binding')
 
     template = """
 !> %(comment)s
 
-    function %(function_name)s %(parameter_list)s bind(C, "%(function_name)s")
-     use, intrinsic%(space)s:: iso_c_binding
-     %(parameter_declarations)s
-     %(return_type_declaration)s
+    function %(function_name)s_c %(parameter_list)s bind(C, name="%(function_name)s")
+     %(use_intrinsic)s
+     %(binding_parameters_declarations)s
+     %(binding_return_type_declaration)s
+    end function %(function_name)s_c
+
+    function %(function_name)s %(parameter_list)s 
+     %(use_intrinsic)s
+     %(fortran_parameters_declarations)s
+     %(fortran_return_type_declaration)s
+
+!>     %(function_name)s = %(function_name)s_c %(parameter_list)s
+
     end function %(function_name)s
 
     """ 
@@ -100,6 +150,5 @@ end module odbql_binding
 
 if __name__ == '__main__':
     s = generateWrappers(decls)
-    print s
-    with open('odbql_binding.f90', 'w') as f: f.write(s)
-
+    with open('odbql_binding.f90', 'w') as f: 
+        f.write(s)
