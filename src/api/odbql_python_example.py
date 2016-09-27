69f222535a6bb7b29c4d17b79e7ec633d99309ab
Piotr Kuchta <peter.kuchta@ecmwf.int>
2016-09-27 11:08:38 +0100

    Improve formatting of the new Python API example. ODB-101: Please use the new API for correct handling of bitfields; the legacy, depracted API will not be fixed.

--- src/api/odbql_python_example.py
+++ src/api/odbql_python_example.py
@@ -8,17 +8,17 @@ Examples of usage of ODB API Python interface.
 
 import odb
 
-""" Example 1. Create a new ODB file.
-"""
+### Example 1. Create a new ODB file.
+###
 
-""" 1.1. Define metadata (column names and their types) 
-    of a new table associated with a physical file 
-    by passing some DDL (Data Definition Language):
-    CREATE TYPE (to define bitfields) and CREATE TABLE
-    statements to function connect.
+### 1.1. Define metadata (column names and their types) 
+#    of a new table associated with a physical file 
+#    by passing some DDL (Data Definition Language):
+#    CREATE TYPE (to define bitfields) and CREATE TABLE
+#    statements to function connect.
+#
+#   Function connect returns a Connection object.
 
-    Function connect returns a Connection object.
-"""
 conn = odb.connect('''
     CREATE TYPE bf AS (f1 bit1, f2 bit2); 
 
@@ -27,42 +27,39 @@ conn = odb.connect('''
     ON 'new_api_example.odb';
 ''')
 
-""" 1.2 Create a Cursor object. """
+### 1.2 Create a Cursor object. """
 c = conn.cursor()
 
-""" 1.3 Populate table with data using SQL INSERT statement 
-    and method Cursor.executemany. 
-"""
+### 1.3 Populate table with data using SQL INSERT statement 
+#    and method Cursor.executemany. 
+
 c.executemany('INSERT INTO foo (x,y,v,status) VALUES (?,?,?,?);', 
                 [[1,0.1, '  one   ', 1], 
                  [2,0.2, '  two   ', 2], 
                  [3,0.3, '  three ', 3], 
                  [4,0.4, '  four  ', 4]])
-""" 1.4 Flush buffers and write to file associated with the table. """
+### 1.4 Flush buffers and write to file associated with the table. """
 conn.commit()
 
 
-"""Example 2. Read contents of a file.
+### Example 2. Read contents of a file.
+#
+#   Note: table foo is associated with a file using CREATE TABLE
+#   DDL statement, see previous example.
 
-   Note: table foo is associated with a file using CREATE TABLE
-   DDL statement, see previous example.
-"""
+### 2.1 Create Cursor object and execute
 
-""" 2.1 Create Cursor object and execute
-"""
-                 
 c = conn.cursor()
 c.execute('select * from foo;')
 
-""" 2.2 Use Cursor.fetchall to retrieve whole result set of SELECT
-    and print its rows using simple print 
-"""
+### 2.2 Use Cursor.fetchall to retrieve whole result set of SELECT
+#    and print its rows using simple print 
+
 for row in c.fetchall():
     print ",".join(str(v) for v in row)
 
 
-""" Example 3. Read a file into Pandas DataFrame object.
-"""
+### Example 3. Read a file into Pandas DataFrame object.
 
 import pandas as pd
 c.execute('select * from foo;')
@@ -72,8 +69,7 @@ d = pd.DataFrame.from_records(c.fetchall(),
 
 print 'Pandas DataFrame:\n', d
 
-""" Example 4. Create numpy array.
-"""
+### Example 4. Create numpy array.
 import numpy as np
 
 c.execute('select x, y, status.f1, status.f2 from foo;')
@@ -81,8 +77,7 @@ a = np.array(c.fetchall())
 print 'numpy array:\n', a
 
 
-""" Example 5. Load some data from MARS or ODB Server into Pandas DataFrame
-"""
+### Example 5. Load some data from MARS or ODB Server into Pandas DataFrame
 conn = odb.connect(ddl = '''
     CREATE TABLE rt16001
     ON "mars://RETRIEVE,
