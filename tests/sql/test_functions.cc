495cbaabf20caaaf5b2ff6fad21c12ba9711619c
Simon Smart <simon.smart@ecmwf.int>
2017-09-07 12:01:12 +0100

    ODB-379: Test edge cases of distance/km functions
    --> i.e. points on top of each other, diametrically imposed, negative
             angles, same angles but differing by 360 degrees, etc.

--- tests/sql/test_functions.cc
+++ tests/sql/test_functions.cc
@@ -40,9 +40,22 @@ CASE("Various distance measuring functions return sensible things") {
 
         row->writeHeader();
 
+        // Include some extreme values
+
         (*row)[0] = 45.0;
         (*row)[1] = 0.0;
+        ++row;
 
+        (*row)[0] = 90.0;
+        (*row)[1] = 180.0;
+        ++row;
+
+        (*row)[0] = -90.0;
+        (*row)[1] = 360.0;
+        ++row;
+
+        (*row)[0] = -45.0;
+        (*row)[1] = -90.0;
         ++row;
     }
 
@@ -62,13 +75,97 @@ CASE("Various distance measuring functions return sensible things") {
         odb::Select oda(sql);
         odb::Select::iterator it = oda.begin();
 
-
         EXPECT((*it)[0] == 1);                                  // Inside relevant great-circle
         EXPECT((*it)[1] == 0);
-        ASSERT(is_approximately_equal((*it)[2], 111120., eps)); // Surface distance to specified point
-        ASSERT(is_approximately_equal((*it)[3], 111.12, eps));  // In m
-        ASSERT((*it)[4] == 0);                                  // Within specified distance of given point
-        ASSERT((*it)[5] == 1);
+        EXPECT(is_approximately_equal((*it)[2], 111120., eps)); // Surface distance to specified point
+        EXPECT(is_approximately_equal((*it)[3], 111.12, eps));  // In m
+        EXPECT((*it)[4] == 0);                                  // Within specified distance of given point
+        EXPECT((*it)[5] == 1);
+        ++it;
+
+        EXPECT((*it)[0] == 0);                                  // Inside relevant great-circle
+        EXPECT((*it)[1] == 0);
+        // pi/4 * R
+        EXPECT(is_approximately_equal((*it)[2], 4889280., eps)); // Surface distance to specified point
+        EXPECT(is_approximately_equal((*it)[3], 4889.28, eps));  // In m
+        EXPECT((*it)[4] == 0);                                  // Within specified distance of given point
+        EXPECT((*it)[5] == 0);
+        ++it;
+
+        EXPECT((*it)[0] == 0);                                  // Inside relevant great-circle
+        EXPECT((*it)[1] == 0);
+        // 3*pi/4 * R
+        EXPECT(is_approximately_equal((*it)[2], 15112320., eps)); // Surface distance to specified point
+        EXPECT(is_approximately_equal((*it)[3], 15112.32, eps));  // In m
+        EXPECT((*it)[4] == 0);                                  // Within specified distance of given point
+        EXPECT((*it)[5] == 0);
+        ++it;
+
+        EXPECT((*it)[0] == 0);                                  // Inside relevant great-circle
+        EXPECT((*it)[1] == 0);
+        EXPECT(is_approximately_equal((*it)[2], 13398177.5541776344, eps)); // Surface distance to specified point
+        EXPECT(is_approximately_equal((*it)[3], 13398.1775541776344, eps));  // In m
+        EXPECT((*it)[4] == 0);                                  // Within specified distance of given point
+        EXPECT((*it)[5] == 0);
+        ++it;
+    }
+
+    // Test against things measured from the north and south poles
+
+    const std::string sql2 = std::string("select "
+                                         "distance(90.0,0.0,lat,lon), "
+                                         "km(90.0,0.0,lat,lon), "
+                                         "distance(-90.0,0.0,lat,lon), "
+                                         "km(-90.0,0.0,lat,lon), "
+                                         "distance(0.0,180.0,lat,lon), "
+                                         "km(0.0,180.0,lat,lon), "
+                                         "from \"") + f.path() + "\";";
+
+    {
+        odb::Select oda(sql2);
+        odb::Select::iterator it = oda.begin();
+
+        // pi/4 * R_e
+        EXPECT(is_approximately_equal((*it)[0], 5000400., eps));
+        EXPECT(is_approximately_equal((*it)[1], 5000.4, eps));
+        // 3*pi/4 * R_e
+        EXPECT(is_approximately_equal((*it)[2], 15001200., eps));
+        EXPECT(is_approximately_equal((*it)[3], 15001.2, eps));
+        // 3*pi/4 * R_e
+        EXPECT(is_approximately_equal((*it)[4], 15001200., eps));
+        EXPECT(is_approximately_equal((*it)[5], 15001.2, eps));
+        ++it;
+
+        EXPECT(is_approximately_equal((*it)[0], 0., eps));
+        EXPECT(is_approximately_equal((*it)[1], 0., eps));
+        // pi * R_e
+        EXPECT(is_approximately_equal((*it)[2], 20001600., eps));
+        EXPECT(is_approximately_equal((*it)[3], 20001.6, eps));
+        // pi / 2 * R_e
+        EXPECT(is_approximately_equal((*it)[4], 10000800., eps));
+        EXPECT(is_approximately_equal((*it)[5], 10000.8, eps));
+        ++it;
+
+        EXPECT(is_approximately_equal((*it)[0], 20001600., eps));
+        EXPECT(is_approximately_equal((*it)[1], 20001.6, eps));
+        // pi * R_e
+        EXPECT(is_approximately_equal((*it)[2], 0., eps));
+        EXPECT(is_approximately_equal((*it)[3], 0., eps));
+        // pi / 2 * R_e
+        EXPECT(is_approximately_equal((*it)[4], 10000800., eps));
+        EXPECT(is_approximately_equal((*it)[5], 10000.8, eps));
+        ++it;
+
+        // 3*pi/4 * R_e
+        EXPECT(is_approximately_equal((*it)[0], 15001200., eps));
+        EXPECT(is_approximately_equal((*it)[1], 15001.2, eps));
+        // pi/4 * R_e
+        EXPECT(is_approximately_equal((*it)[2], 5000400., eps));
+        EXPECT(is_approximately_equal((*it)[3], 5000.4, eps));
+        // pi / 2 * R_e
+        EXPECT(is_approximately_equal((*it)[4], 10000800., eps));
+        EXPECT(is_approximately_equal((*it)[5], 10000.8, eps));
+        ++it;
     }
 }
 
