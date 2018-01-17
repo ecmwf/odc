605c027b3fe7069c308729b82bc1776064dcc250
Simon Smart <simon.smart@ecmwf.int>
2018-01-17 16:04:59 +0000

    ODB-401: Add tests for odb import and SQL variables in odb sql

---
+++ tests/tools/test_odb_import.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -uex
+
+# A unique working directory
+
+wd=$(pwd)
+test_wd=$(pwd)/test_odb_import
+
+mkdir -p ${test_wd}
+cd ${test_wd}
+
+# In case we are resuming from a previous failed run, which has left output in the directory
+rm *.odb || true
+
+# Create some test data
+
+cat > data.csv <<EOF
+col1:INTEGER,col2:REAL,col3:DOUBLE,col4:INTEGER,col4:BITFIELD[a:1;b:2;c:5]
+1,1.23,4.56,7,999
+123,0.0,0.0,321,888
+321,0.0,0.0,123,777
+0,3.25,0.0,0,666
+0,0.0,3.25,0,555
+EOF
+
+odb import data.csv data.odb
+
+[[ $(odb count data.odb) -eq 5 ]] || (echo "Unexpected number of rows in ODB" ; false)
+
+# Check that we get what we are expecting
+
+# n.b. It is important that TABS are literally correct in this output.
+# Beware editors that change them by default
+
+cat > odb_ls_reference <<EOF
+col1	col2	col3	col4	col4	
+1	1.230000	4.560000	999	999	
+123	0.000000	0.000000	888	888	
+321	0.000000	0.000000	777	777	
+0	3.250000	0.000000	666	666	
+0	0.000000	3.250000	555	555	
+EOF
+
+odb ls data.odb > odb_ls
+
+cmp odb_ls_reference odb_ls
+
+# Clean up
+
+cd ${wd}
+rm -rf ${test_wd}
---
+++ tests/tools/test_odb_sql_variables.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+set -uex
+
+# A unique working directory
+
+wd=$(pwd)
+test_wd=$(pwd)/test_odb_sql_variables
+
+mkdir -p ${test_wd}
+cd ${test_wd}
+
+# In case we are resuming from a previous failed run, which has left output in the directory
+rm *.odb || true
+
+
+# Test that variables defined in the schema can be used in queries.
+# See ODB-127
+
+cat > schema.hh <<EOF
+SET \$var1 = 123;
+SET \$var2 = 3.24;
+EOF
+
+export ODB_API_SCHEMA_PATH="$(pwd)/schema.hh"
+
+
+# Create some test data
+
+cat > data.csv <<EOF
+col1:INTEGER,col2:REAL,col3:DOUBLE,col4:INTEGER
+1,1.23,4.56,7
+123,0.0,0.0,321
+321,0.0,0.0,123
+0,3.25,0.0,0
+0,0.0,3.25,0
+EOF
+
+odb import data.csv data.odb
+
+# Runs some selects, to see that the data is correct
+
+odb sql 'select * where col1=1' -i data.odb -f odb -o outfile.odb
+
+cat > expect.csv <<EOF
+col1:INTEGER,col2:REAL,col3:DOUBLE,col4:INTEGER
+1,1.23,4.56,7
+EOF
+odb import expect.csv expect.odb
+odb compare outfile.odb expect.odb
+
+# --
+
+odb sql 'select * where col1=$var1' -i data.odb -f odb -o outfile.odb
+
+cat > expect.csv <<EOF
+col1:INTEGER,col2:REAL,col3:DOUBLE,col4:INTEGER
+123,0.0,0.0,321
+EOF
+odb import expect.csv expect.odb
+odb compare outfile.odb expect.odb
+
+# --
+
+odb sql 'select * where col2=$var1' -i data.odb -f odb -o outfile.odb
+
+[[ -e outfile.odb && ! -s outfile.odb ]] # Expect zero sized file
+
+# --
+
+odb sql 'select * where col3=$var1' -i data.odb -f odb -o outfile.odb
+
+[[ -e outfile.odb && ! -s outfile.odb ]] # Expect zero sized file
+
+# --
+
+odb sql 'select * where col4=$var1' -i data.odb -f odb -o outfile.odb
+
+cat > expect.csv <<EOF
+col1:INTEGER,col2:REAL,col3:DOUBLE,col4:INTEGER
+321,0.0,0.0,123
+EOF
+odb import expect.csv expect.odb
+odb compare outfile.odb expect.odb
+
+# --
+
+odb sql 'select * where col1<$var2' -i data.odb -f odb -o outfile.odb
+
+cat > expect.csv <<EOF
+col1:INTEGER,col2:REAL,col3:DOUBLE,col4:INTEGER
+1,1.23,4.56,7
+0,3.25,0.0,0
+0,0.0,3.25,0
+EOF
+odb import expect.csv expect.odb
+odb compare outfile.odb expect.odb
+
+# --
+
+odb sql 'select * where col2<$var2' -i data.odb -f odb -o outfile.odb
+
+cat > expect.csv <<EOF
+col1:INTEGER,col2:REAL,col3:DOUBLE,col4:INTEGER
+1,1.23,4.56,7
+123,0.0,0.0,321
+321,0.0,0.0,123
+0,0.0,3.25,0
+EOF
+odb import expect.csv expect.odb
+odb compare outfile.odb expect.odb
+
+# --
+
+odb sql 'select * where col3<$var2' -i data.odb -f odb -o outfile.odb
+
+cat > expect.csv <<EOF
+col1:INTEGER,col2:REAL,col3:DOUBLE,col4:INTEGER
+123,0.0,0.0,321
+321,0.0,0.0,123
+0,3.25,0.0,0
+EOF
+odb import expect.csv expect.odb
+odb compare outfile.odb expect.odb
+
+# --
+
+odb sql 'select * where col4<$var2' -i data.odb -f odb -o outfile.odb
+
+cat > expect.csv <<EOF
+col1:INTEGER,col2:REAL,col3:DOUBLE,col4:INTEGER
+0,3.25,0.0,0
+0,0.0,3.25,0
+EOF
+odb import expect.csv expect.odb
+odb compare outfile.odb expect.odb
+
+
+# Clean up
+
+cd ${wd}
+rm -rf ${test_wd}
