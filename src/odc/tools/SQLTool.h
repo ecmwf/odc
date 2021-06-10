6707c98073bc09d3ec7ccc2838e3011351da1fef
Simon Smart <simon.smart@ecmwf.int>
2021-06-10 15:42:12 +0100

    ODB-521: Fix writing sql tool output to CSV file

--- src/odc/tools/SQLTool.h
+++ src/odc/tools/SQLTool.h
@@ -50,20 +50,18 @@ public:
 	static void usage(const std::string& name, std::ostream &o)
 	{
 		o << name << " <select-statement> | <script-filename>" << std::endl;
-        o << "             [-T]                  Disables printing of column names" << std::endl;
-        o << "             [-offset <offset>]    Start processing file at a given offset" << std::endl;
-        o << "             [-length <length>]    Process only given bytes of data" << std::endl;
-        o << "             [-N]                  Do not write NULLs, but proper missing data values" << std::endl;
-        o << "             [-i <inputfile>]      ODB input file" << std::endl;
-        o << "             [-o <outputfile>]     ODB output file" << std::endl;
-        o << "             [-f default|wide|odb] ODB output format (default is ascii; odb is binary ODB, wide is ascii with"
-												<< " bitfields definitions in header)" << std::endl;
-        o << "             [-delimiter <delim>]  Changes the default values' delimiter (TAB by default)" << std::endl; 
-        o << "                                   delim can be any character or string" << std::endl;
-        o << "             [--binary|--bin]      Print bitfields in binary notation" << std::endl;
-//        o << "             [--hexadecimal|--hex] Print bitfields in hexadecimal notation" << std::endl;
-        o << "             [--no_alignment]      Do not align columns" << std::endl;
-        o << "             [--full_precision]    Print with full precision" << std::endl;
+        o << "             [-T]                        Disables printing of column names" << std::endl;
+        o << "             [-offset <offset>]          Start processing file at a given offset" << std::endl;
+        o << "             [-length <length>]          Process only given bytes of data" << std::endl;
+        o << "             [-N]                        Do not write NULLs, but proper missing data values" << std::endl;
+        o << "             [-i <inputfile>]            ODB input file" << std::endl;
+        o << "             [-o <outputfile>]           ODB output file" << std::endl;
+        o << "             [-f default|wide|ascii|odb] ODB output format (odb is binary ODB, ascii and wide are ascii formatted with bitfield definitions in header. Default is ascii on stdout and odb to file)" << std::endl;
+        o << "             [-delimiter <delim>]        Changes the default values' delimiter (TAB by default)" << std::endl; 
+        o << "                                         delim can be any character or string" << std::endl;
+        o << "             [--binary|--bin]            Print bitfields in binary notation" << std::endl;
+        o << "             [--no_alignment]            Do not align columns" << std::endl;
+        o << "             [--full_precision]          Print with full precision" << std::endl;
 	}
 
 private:
